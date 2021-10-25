74ed5ff805fe78631eaa72d245a0548d8fb4e098
mihlali dzingwa <mihlalidzingwa2@gmail.com>
2021-10-25 16:57:51 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const db = require('./db')
+
+const bodyParser = require("body-parser");
+//const { check, validationResult } = require("express-validator");
+
+const cors = require("cors")
+app.use(cors())
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const port = 8080;
+
+
+// GET
+app.get('/tasks', async (req, res) => {
+    try {
+        const result = await db.pool.query("select * from tasks");
+        res.send(result);
+    } catch (err) {
+        throw err;
+    }
+});
+ 
+// POST
+app.post('/tasks', async (req, res) => {
+    let task = req.body;
+    try {
+        const result = await db.pool.query("insert into tasks (description) values (?)", [task.description]);
+        res.send(result);
+    } catch (err) {
+        throw err;
+    }
+});
+ 
+app.put('/tasks', async (req, res) => {
+    let task = req.body;
+    try {
+        const result = await db.pool.query("update tasks set description = ?, completed = ? where id = ?", [task.description, task.completed, task.id]);
+        res.send(result);
+    } catch (err) {
+        throw err;
+    } 
+});
+ 
+app.delete('/tasks', async (req, res) => {
+    let id = req.query.id;
+    try {
+        const result = await db.pool.query("delete from tasks where id = ?", [id]);
+        res.send(result);
+    } catch (err) {
+        throw err;
+    } 
+});
+ 
+app.listen(port, () => console.log(`Listening on port ${port}`));
